a3d9617815f58f85017dcae9e0ddc0e86b5fa8bb
FMinister <johannes.leyrer+git@googlemail.com>
2023-04-03 18:13:40 +0200

    deleted provider from main

--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,30 @@ import 'package:co2app/providers/message_provider.dart';
 import 'package:co2app/screens/line_chart_screen.dart';
 import 'package:co2app/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'components/color_schemes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => ThemeProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => DataProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => MessageProvider(),
-        ),
-      ],
-      child: Consumer<ThemeProvider>(
-        builder: (ctx, themeProv, _) => Consumer<MessageProvider>(
-          builder: (ctx, messageProvider, _) => MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: "CO2Monitor",
-              theme: themeProv.isDark
-                  ? ThemeData(useMaterial3: true, colorScheme: lightColorScheme)
-                  : ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-              home: const LineChartScreen()),
-        ),
-      ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "CO2Monitor",
+      // theme: themeProv.isDark
+      //     ? ThemeData(useMaterial3: true, colorScheme: lightColorScheme)
+      //     : ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      home: const LineChartScreen(),
     );
   }
 }
