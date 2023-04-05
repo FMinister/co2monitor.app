e04466d4505158ec8b40fb3309511ecfc7727309
FMinister <johannes.leyrer+git@googlemail.com>
2023-04-05 13:33:47 +0200

    started to change app drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,5 @@
-import 'package:co2app/providers/data_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers/message_provider.dart';
 
@@ -8,42 +7,14 @@ class AppDrawer extends StatefulWidget {
   const AppDrawer({super.key});
 
   @override
-  State<AppDrawer> createState() => _AppDrawerState();
+  AppDrawerState createState() => AppDrawerState();
 }
 
-class _AppDrawerState extends State<AppDrawer> {
+class AppDrawerState extends ConsumerState<AppDrawer> {
   final periods = [1, 3, 6, 12, 24, 48];
   var _isLoading = false;
 
-  Future<void> onTabPeriod(BuildContext context, int period) async {
-    final messageProvider =
-        Provider.of<MessageProvider>(context, listen: false);
-
-    try {
-      setState(() {
-        _isLoading = true;
-      });
-      messageProvider.hideSnackBar(context);
-      messageProvider.showLoadingSnackBar(context);
-      await Provider.of<DataProvider>(context, listen: false).setPeriod(period);
-      if (context.mounted) {
-        messageProvider.showSuccessSnackBar(
-            context, "Data successfully refreshed!");
-      }
-    } catch (error) {
-      setState(() {
-        _isLoading = false;
-      });
-      messageProvider.showErrorSnackBar(context, error.toString());
-      Scaffold.of(context).closeDrawer();
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-      messageProvider.hideSnackBar(context);
-      Scaffold.of(context).closeDrawer();
-    }
-  }
+  Future<void> onTabPeriod(int period) async {}
 
   @override
   Widget build(BuildContext context) {
@@ -92,7 +63,7 @@ class _AppDrawerState extends State<AppDrawer> {
                         fontWeight: FontWeight.bold,
                         fontSize: 16),
                   ),
-                  onTap: () async => await onTabPeriod(context, periods[index]),
+                  onTap: () async => await onTabPeriod(periods[index]),
                 );
               }),
           if (_isLoading)
